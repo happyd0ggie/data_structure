4478b1fbd1386cb58fc6d6ee4e82f8a718068db2
dexiang sheng <deyoung_shen@msn.cn>
2016-08-30 22:13:21 +0800

    update insert_sort

--- insert_sort.c
+++ insert_sort.c
@@ -1,54 +1,35 @@
-#include "common.h"
+#include "insert_sort.h"
 
-int main(int argc, char const *argv[])
-{
-	int arr[] = {2, 8, 7, 1, 3, 5, 6};
-    printf("Before sorting...\n");
-    for (int i = 0; i < 7; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\nAfter sorting...\n");
-	insert_sort(arr, 7);
-    for (int i = 0; i < 7; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-	return 0;
-}
-
-void insert_sort(int arr[], int len)
+void insert_sort(int *nums, int nums_size)
 {
 	int i = 0;
-    int j;
-    int k;
-    int tmp;
-	
-	while (i < len - 1)
-    {
-        j = 0;
-        k = i + 1;
-        tmp = arr[k];
-        
-        while (j <= i && tmp > arr[j])
-        {
-            j++;
-        }
-        if (j > i)
-        {
-            i++;
-            continue;
-        }
-        while (k > j)
-        {
-            arr[k] = arr[k - 1];
-            k--;
-        }
-        arr[k] = tmp;
-        i++;
-    }
+	int j;
+	int k;
+	int tmp;
 
-    return;
+	while (i < nums_size - 1)
+	{
+		j = 0;
+		k = i + 1;
+		tmp = nums[k];
+
+		while (j <= i && tmp > nums[j])
+		{
+			j++;
+		}
+		if (j > i)
+		{
+			i++;
+			continue;
+		}
+		while (k > j)
+		{
+			nums[k] = nums[k - 1];
+			k--;
+		}
+		nums[k] = tmp;
+		i++;
+	}
+
+	return;
 }
---
+++ insert_sort.h
@@ -0,0 +1,3 @@
+#include "common.h"
+
+void insert_sort(int *nums, int nums_size);
